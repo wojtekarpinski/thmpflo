8b0d947c0ca71563984158ae3a663327d8315366
wojtekarpinski <71265945+wojtekarpinski@users.noreply.github.com>
2023-11-14 15:20:11 +0100

    Added search form

---
+++ scripts/searchSelect.js
@@ -0,0 +1,22 @@
+function searchSelect() {
+    console.log("function init");
+    var basicField = document.getElementById("basic-search");
+    var advancedFields = document.getElementById("advanced-search");
+    var searchSwitch = document.getElementById("search-selector");
+    var searchForm = document.getElementById("search");
+
+
+    if (basicField.style.display === "none") {
+        searchForm.action = "/php/basicSearch.php"
+        advancedFields.style.display = "none";
+        basicField.style.display = "flex";
+        searchSwitch.innerHTML = "Wysukiwanie zaawansowane 🔽";
+    } else {
+        basicField.style.display = "none";
+        searchForm.action ="/php/advancedSearch.php"
+        advancedFields.style.display = "flex";
+        searchSwitch.innerHTML = "Proste wyszukiwanie 🔼";
+    }
+
+
+}
