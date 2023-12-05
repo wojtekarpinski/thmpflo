b8b66454a54df574bcaf999b386fe2b8704b78b7
Wojciech Karpiński <wkarpinski@nowykampus.ckc.uw.edu.pl>
2023-12-05 14:26:16 +0100

    Added: search form twig Changed: frontpage searchform Overriden: btn - primary

--- scripts/searchSelect.js
+++ scripts/searchSelect.js
@@ -9,12 +9,12 @@ function searchSelect() {
     if (basicField.style.display === "none") {
         // searchForm.action = "/php/basicSearch.php"
         advancedFields.style.display = "none";
-        basicField.style.display = "flex";
+        basicField.style.display = "block";
         searchSwitch.innerHTML = "Wysukiwanie zaawansowane <span class=\"caret\"></span>";
     } else {
         basicField.style.display = "none";
         // searchForm.action ="/php/advancedSearch.php"
-        advancedFields.style.display = "flex";
+        advancedFields.style.display = "block";
         searchSwitch.innerHTML = "Proste wyszukiwanie <span class=\"dropup\"><span class=\"caret\"></span></span>";
     }
 
