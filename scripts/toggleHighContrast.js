d2445650f696a6739cc865a106f43e412f3f19fd
Wojciech Karpiński <wkarpinski@nowykampus.ckc.uw.edu.pl>
2023-12-19 10:55:18 +0100

    LARGER TEXT, USER PROFILE LOOKS

--- scripts/toggleHighContrast.js
+++ scripts/toggleHighContrast.js
@@ -1,17 +1,17 @@
 Drupal.behaviors.toggleHighContrast = {
     attach: function toggleHighContrast() {
-        console.log("highContrast value: ", localStorage.getItem("highContrast"))
+        // console.log("highContrast value: ", localStorage.getItem("highContrast"))
         if(localStorage.getItem("highContrast") == null) {
-            console.log("creating highContrast in localStorage");
+            // console.log("creating highContrast in localStorage");
             localStorage.setItem("highContrast", JSON.stringify(false));
         }
         
         function switcher() {
             if(JSON.parse(localStorage.getItem("highContrast")) === true) {
-                console.log("Adding class to body");
+                // console.log("Adding class to body");
                 document.body.classList.add("high-contrast");
             } else {
-                console.log("Removing class from body");
+                // console.log("Removing class from body");
                 document.body.classList.remove("high-contrast");
     
             }
@@ -19,15 +19,15 @@ Drupal.behaviors.toggleHighContrast = {
         switcher();
 
         document.getElementById("toggle-high-contrast").addEventListener("click",function() {
-            console.log("event detected, actual localStorage value is: ", localStorage.getItem("highContrast"));
+            // console.log("event detected, actual localStorage value is: ", localStorage.getItem("highContrast"));
             if(JSON.parse(localStorage.getItem("highContrast")) === false) {
-                console.log("highContrast was false ->", localStorage.getItem("highContrast"));
+                // console.log("highContrast was false ->", localStorage.getItem("highContrast"));
                 localStorage.setItem("highContrast", JSON.stringify(true));
-                console.log("highContrast set to true ->", localStorage.getItem("highContrast"));
+                // console.log("highContrast set to true ->", localStorage.getItem("highContrast"));
             } else {
-                console.log("highContrast was true ->", localStorage.getItem("highContrast"));
+                // console.log("highContrast was true ->", localStorage.getItem("highContrast"));
                 localStorage.setItem("highContrast", JSON.stringify(false));
-                console.log("highContrast set to false ->", localStorage.getItem("highContrast"));
+                // console.log("highContrast set to false ->", localStorage.getItem("highContrast"));
             }
             switcher();
 
