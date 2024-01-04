060cd5fc1a4ecd0e37c1c8389f3f6cfd2b36d707
Wojciech Karpiński <wkarpinski@nowykampus.ckc.uw.edu.pl>
2024-01-04 12:31:01 +0100

    Disabled debug console.logs

--- scripts/toggleLargerText.js
+++ scripts/toggleLargerText.js
@@ -1,18 +1,18 @@
 Drupal.behaviors.toggleLargerText = {
     attach: function toggleLargerText() {
         if(localStorage.getItem("largerText") == null) {
-            console.log("creating largerText in localStorage");
+            //console.log("creating largerText in localStorage");
             localStorage.setItem("largerText", JSON.stringify(false));
         }
-        console.log(localStorage.largerText);
+        //console.log(localStorage.largerText);
 
         function switcher() {
-            console.log("switcher init");
+            //console.log("switcher init");
             if(JSON.parse(localStorage.getItem("largerText")) === true) {
-                console.log("Adding class to body");
+                //console.log("Adding class to body");
                 document.body.classList.add("larger-text");
             } else {
-                console.log("Removing class from body");
+                //console.log("Removing class from body");
                 document.body.classList.remove("larger-text");
     
             }
@@ -20,15 +20,15 @@ Drupal.behaviors.toggleLargerText = {
         switcher();
 
         document.getElementById("toggle-larger-text").addEventListener("click",function() {
-            console.log("Setting largerText to true: ", localStorage.largerText);
+           // console.log("Setting largerText to true: ", localStorage.largerText);
             localStorage.setItem("largerText", JSON.stringify(true));
-            console.log("Done: ", localStorage.largerText);
+            //console.log("Done: ", localStorage.largerText);
             switcher();
         });
         document.getElementById("toggle-regular-text").addEventListener("click",function() {
-            console.log("Setting largerText to false: ", localStorage.largerText);
+            //console.log("Setting largerText to false: ", localStorage.largerText);
             localStorage.setItem("largerText", JSON.stringify(false));
-            console.log("Done: ", localStorage.largerText);
+            //console.log("Done: ", localStorage.largerText);
             switcher();
             });
         
--- scripts/toggleSmallerText.js
+++ scripts/toggleSmallerText.js
@@ -1,18 +1,18 @@
 Drupal.behaviors.toggleSmallerText = {
     attach: function toggleSmallerText() {
         if(localStorage.getItem("smallerText") == null) {
-            console.log("creating smallerText in localStorage");
+            //console.log("creating smallerText in localStorage");
             localStorage.setItem("smallerText", JSON.stringify(false));
         }
-        console.log(localStorage.smallerText);
+        //console.log(localStorage.smallerText);
 
         function switcher() {
-            console.log("switcher init");
+            //console.log("switcher init");
             if(JSON.parse(localStorage.getItem("smallerText")) === true) {
-                console.log("Adding class to body");
+                //console.log("Adding class to body");
                 document.body.classList.add("smaller-text");
             } else {
-                console.log("Removing class from body");
+                //console.log("Removing class from body");
                 document.body.classList.remove("smaller-text");
     
             }
@@ -20,21 +20,21 @@ Drupal.behaviors.toggleSmallerText = {
         switcher();
 
         document.getElementById("toggle-smaller-text").addEventListener("click",function() {
-            console.log("Setting smallerText to true: ", localStorage.smallerText);
+            //console.log("Setting smallerText to true: ", localStorage.smallerText);
             localStorage.setItem("smallerText", JSON.stringify(true));
-            console.log("Done: ", localStorage.smallerText);
+            //console.log("Done: ", localStorage.smallerText);
             switcher();
         });
         document.getElementById("toggle-regular-text").addEventListener("click",function() {
-            console.log("Setting smallerText to false: ", localStorage.smallerText);
+            //console.log("Setting smallerText to false: ", localStorage.smallerText);
             localStorage.setItem("smallerText", JSON.stringify(false));
-            console.log("Done: ", localStorage.smallerText);
+            //console.log("Done: ", localStorage.smallerText);
             switcher();
             });
         document.getElementById("toggle-larger-text").addEventListener("click",function() {
-            console.log("Setting smallerText to false: ", localStorage.smallerText);
+            //console.log("Setting smallerText to false: ", localStorage.smallerText);
             localStorage.setItem("smallerText", JSON.stringify(false));
-            console.log("Done: ", localStorage.smallerText);
+            //console.log("Done: ", localStorage.smallerText);
             switcher();
             });
         
