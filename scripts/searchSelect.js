46c03926435bf2b8014b8e2597d4fa03ffdd1f88
Wojciech Karpiński <wkarpinski@nowykampus.ckc.uw.edu.pl>
2024-01-30 18:40:02 +0100

    Modified: search selector

--- scripts/searchSelect.js
+++ scripts/searchSelect.js
@@ -1,20 +1,27 @@
 function searchSelect() {
     console.log("function init");
-    var basicField = document.getElementById("basic-search");
-    var advancedFields = document.getElementById("advanced-search");
+    var searchFormItems = document.getElementsByClassName("form-item");
+    var basicField = searchFormItems[0];
+    let inputs = searchFormItems.length;
+    for(let i = 1; i <= 7; i++) {
+        advancedFields[i-1]= searchFormItems[i];
+    }
+
     var searchSwitch = document.getElementById("search-selector");
     var searchForm = document.getElementById("search");
 
 
     if (basicField.style.display === "none") {
-        // searchForm.action = "/php/basicSearch.php"
-        advancedFields.style.display = "none";
+        for(let i = 1; i <= inputs; i++) {
+            searchFormItems[i].style.display="none"
+        }
         basicField.style.display = "block";
         searchSwitch.innerHTML = "Wysukiwanie zaawansowane <span class=\"caret\"></span>";
     } else {
         basicField.style.display = "none";
-        // searchForm.action ="/php/advancedSearch.php"
-        advancedFields.style.display = "block";
+        for(let i = 1; i <= inputs; i++) {
+            searchFormItems[i].style.display="block"
+        }
         searchSwitch.innerHTML = "Proste wyszukiwanie <span class=\"dropup\"><span class=\"caret\"></span></span>";
     }
 
