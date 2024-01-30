71e4562b853ca51e3e4e76bca1887611c6d3c0d8
Wojciech Karpiński <wkarpinski@nowykampus.ckc.uw.edu.pl>
2024-01-30 19:38:18 +0100

    Working search selector

--- scripts/searchSelect.js
+++ scripts/searchSelect.js
@@ -3,24 +3,29 @@ function searchSelect() {
     var searchFormItems = document.getElementsByClassName("form-item");
     var basicField = searchFormItems[0];
     let inputs = searchFormItems.length;
-    for(let i = 1; i <= 7; i++) {
-        advancedFields[i-1]= searchFormItems[i];
-    }
+    let byebyeLabel = document.querySelector("label");
+    let discipline = document.querySelector("#edit-fudn")
 
     var searchSwitch = document.getElementById("search-selector");
-    var searchForm = document.getElementById("search");
+    
 
-
-    if (basicField.style.display === "none") {
-        for(let i = 1; i <= inputs; i++) {
+    console.log(getComputedStyle(basicField).getPropertyValue("display"));
+    if (getComputedStyle(basicField).getPropertyValue("display") === "none") {
+        for(let i = 1; i < inputs; i++) {
+            console.log(i);
             searchFormItems[i].style.display="none"
         }
         basicField.style.display = "block";
         searchSwitch.innerHTML = "Wysukiwanie zaawansowane <span class=\"caret\"></span>";
     } else {
         basicField.style.display = "none";
-        for(let i = 1; i <= inputs; i++) {
+        for(let i = 1; i < inputs; i++) {
             searchFormItems[i].style.display="block"
+        }
+        if(getComputedStyle(byebyeLabel).getPropertyValue("display") != "none")
+        {
+            byebyeLabel.style.display = "none";
+            discipline.setAttribute("placeholder","Dyscyplina naukowa");
         }
         searchSwitch.innerHTML = "Proste wyszukiwanie <span class=\"dropup\"><span class=\"caret\"></span></span>";
     }
